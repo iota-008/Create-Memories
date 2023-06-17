98f11cfb99a75057e9ed9507d47cb0c28d5d4d98
Ankit Raibole <46680697+iota-008@users.noreply.github.com>
2023-06-17 22:11:41 +0000

    updates

--- memories/src/api/post.js
+++ memories/src/api/post.js
@@ -1,33 +1,36 @@
 import axios from "axios";
 const url = "https://memories-backend-z796.onrender.com/posts";
-var token = localStorage.getItem("auth-token");
+// const url = "http://127.0.0.1:5000/posts"
+var token = localStorage.getItem( "auth-token" );
 
 //* api for fetching posts
 export const fetchPosts = () =>
-	axios.get(url, { headers: { "auth-token": `Bearer ${token}` } });
+	axios.get( url, { headers: { "auth-token": `Bearer ${ token }` } } );
 
 //* api to create a post
-export const createPost = (newPost) =>
-	axios.post(url, newPost, { headers: { "auth-token": `Bearer ${token}` } });
+export const createPost = ( newPost ) =>
+	axios.post( url, newPost, { headers: { "auth-token": `Bearer ${ token }` } } );
 
 //* api to update posts
-export const updatePost = (id, updatedPost) =>
-	axios.patch(`${url}/${id}`, updatedPost, {
-		headers: { "auth-token": `Bearer ${token}` },
-	});
+export const updatePost = ( id, updatedPost ) =>
+	axios.patch( `${ url }/${ id }`, updatedPost, {
+		headers: { "auth-token": `Bearer ${ token }` },
+	} );
 
 //* api to delete posts
-export const deletePost = (id) =>
-	axios.delete(`${url}/${id}`, {
-		headers: { "auth-token": `Bearer ${token}` },
-	});
+export const deletePost = ( id ) =>
+	axios.delete( `${ url }/${ id }`, {
+		headers: { "auth-token": `Bearer ${ token }` },
+	} );
 
 //* api to like posts
-export const likePost = (id) =>
-	axios.patch(
-		`${url}/${id}/likePost`,
+export const likePost = ( id ) =>
+{
+	return axios.patch(
+		`${ url }/${ id }/likePost`,
 		{},
 		{
-			headers: { "auth-token": `Bearer ${token}` },
+			headers: { "auth-token": `Bearer ${ token }` },
 		}
 	);
+}
--- memories/src/api/user.js
+++ memories/src/api/user.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 const url = "https://memories-backend-z796.onrender.com/user";
+// const url = "http://127.0.0.1:5000/user"
 
 
 //* api to register user
-export const registerUser = (userData) =>
-	axios.post(`${url}/register`, userData);
+export const registerUser = ( userData ) =>
+	axios.post( `${ url }/register`, userData );
 
 //* api to login user
 export const loginUser = ( userData ) => axios.post( `${ url }/login`, userData );
